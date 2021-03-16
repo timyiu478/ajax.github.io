f0f1f44a09846fa5ae3064fbd192c4f48edf2dd2
timyiu478 <45873712+timyiu478@users.noreply.github.com>
2021-03-16 23:25:38 +0800

    sdf

---
+++ ajax-get-data.js
@@ -0,0 +1,17 @@
+var xhr = new XMLHttpRequest();
+xhr.open('get','https://timyiu478.github.io/ajax.github.io/ajax-data.json',true);
+xhr.send();
+
+xhr.onload = function(){
+  //console.log(xhr.responseText);
+  //判斷 HTTP 狀態碼是否成功連線
+  if(xhr.status == 200){
+  //把抓到的資料加以運用
+  var str = JSON.parse(xhr.responseText);
+  //選取 DOM，並渲染至網頁
+  document.querySelector('#ajax_data_name').textContent = str[0].names;
+  } else{
+    console.log('資料讀取錯誤!!');
+  }
+  
+}
